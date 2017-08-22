21154e4095a41519d7bdeec64b8ca5166f0a35f7
Pavel Nedelko <pnedelko@gmail.com>
2017-08-22 14:16:35 +0200

    fixed function name

--- src/jsonata-moment.js
+++ src/jsonata-moment.js
@@ -3,10 +3,10 @@
 var moment = require('moment');
 var jsonata = require('jsonata');
 
-function elasticJsonata(jsonataExpr, options) {
+function jsonataMoment(jsonataExpr, options) {
     var expr = jsonata(jsonataExpr, options);
     expr.assign('moment', (arg1, arg2, arg3, arg4) => moment(arg1, arg2, arg3, arg4));
     return expr;
 }
 
-module.exports = elasticJsonata;
+module.exports = jsonataMoment;
